1b09a5c85d25c2dec9f2dd1ee7971448a947005f
HyunM <wisek0226@gmail.com>
2020-06-26 08:20:46 -0700

    it has a dely so I have to clcik 4 times at first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfClicks: 0
+        }
+    }
+
+    componentDidMount() {
+        if(this.state.numberOfClicks > 18){
+            this.setState({numberOfClicks: 0});
+        }
+    }
+
+    componentDidUpdate() {
+        if(this.state.numberOfClicks > 18){
+            this.setState({numberOfClicks: 0});
+        }
+    }
+
+    changeButtonColor = () => {
+        const idOfHotButton = document.getElementById("hotButton");
+        this.setState( {numberOfClicks: this.state.numberOfClicks + 1 } );
+
+        console.log(this.state.numberOfClicks);
+
+        if(this.state.numberOfClicks === 3){
+            idOfHotButton.className = "second";
+        } else if (this.state.numberOfClicks === 6) {
+            idOfHotButton.className = "third";
+        } else if (this.state.numberOfClicks === 9) {
+            idOfHotButton.className = "fourth";
+        } else if (this.state.numberOfClicks === 12) {
+            idOfHotButton.className = "fifth";
+        } else if (this.state.numberOfClicks === 15) {
+            idOfHotButton.className = "sixth";
+        } else if (this.state.numberOfClicks === 18) {
+            idOfHotButton.className = "first";
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hot Button Test</h1>
+                <input id="hotButton" className="first" type="button" value="Hot Button" onClick={this.changeButtonColor}/>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
